c4529622bccb19c882cb194d93353d9f72f1f1c2
Austin <aust.bagley@gmail.com>
2024-09-02 14:56:35 -0600

    style planets into cards

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -14,19 +14,29 @@ export default function Root() {
         _getPlanets();
     }, []);
 
-    function List () {
-        const listItems = planets.map((planet: any, index: any) =>
-            <li key={index}>{planet.name}</li>
+    const PlanetGrid = ({ planet }: any) => {
+        return (
+            <div className="grid grid-cols-3 bg-slate-700 text-white p-4 my-4 rounded-lg shadow-lg">
+                <div className="col-span-2">
+                    <h2 className="text-xl font-semibold mb-2">{planet.name}</h2>
+                    <p><span className="font-bold">Population:</span> {planet.population}</p>
+                    <p><span className="font-bold">Climate:</span> {planet.climate}</p>
+                </div>
+                <div className="content-center text-center">
+                    <button className="mx-auto px-4 py-2 bg-white text-slate-700 hover:text-slate-400 cursor-pointer rounded">View Residents</button>
+                </div>
+            </div>
         );
-        return <ul>{listItems}</ul>;
-    }
+      };
 
     return (
         <>
         <Nav />
         <div className="container mx-auto pt-4">
-            <h1 className="text-3xl font-bold underline">Hello World!</h1>
-            <List />
+            <h1 className="text-3xl font-bold text-slate-800">The Planets of Star Wars</h1>
+            {planets.map((planet, index) => (
+                <PlanetGrid key={index} planet={planet} />
+            ))}
         </div></>
     )
 }

687280234391eb949e8923eb0438a117ca938d47
Austin <aust.bagley@gmail.com>
2024-09-02 16:43:40 -0600

    display resident info

--- src/routes/resident.tsx
+++ src/routes/resident.tsx
@@ -9,8 +9,15 @@ export default function Resident() {
         <Nav />
         <div className="container mx-auto pt-4">
             <h1 className="text-3xl font-bold text-slate-800">Hello {state.name}!</h1>
-            {/* TODO: show resident information/details */}
             {/* TODO: display breadcrumbs */}
+            <div className="grid grid-cols-1 border-solid border-2 border-slate-800 rounded-lg p-4 mt-4">
+                <p><span className="font-bold">Height:</span> {state.height}</p>
+                <p><span className="font-bold">Mass:</span> {state.mass}</p>
+                <p><span className="font-bold">Hair Color:</span> {state.hair_color}</p>
+                <p><span className="font-bold">Skin Color:</span> {state.skin_color}</p>
+                <p><span className="font-bold">Eye Color:</span> {state.eye_color}</p>
+                <p><span className="font-bold">Gender:</span> {state.gender}</p>
+            </div>
         </div>
         </>
     )
